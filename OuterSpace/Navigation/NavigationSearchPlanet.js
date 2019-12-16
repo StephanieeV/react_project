6af8e48fe3025ce03954d96ce15a70ebdf54a519
Lucie Perruchaud <lucie.perruchaud@gmail.com>
2019-12-16 16:59:14 +0100

    ajout du gif de chargement

--- OuterSpace/Navigation/NavigationSearchPlanet.js
+++ OuterSpace/Navigation/NavigationSearchPlanet.js
@@ -2,7 +2,7 @@ import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 import SearchPlanet from "../Component/SearchPlanet";
 import Accueil from "../Component/Accueil";
-import Accueil from "../Component/MeteoMars";
+import MeteoMars from "../Component/MeteoMars";
 
 const SearchStackNavigator = createStackNavigator({
   SearchPlanet: {
