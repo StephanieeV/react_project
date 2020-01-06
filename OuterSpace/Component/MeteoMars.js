1e68f0f8f569feeabcbe56ec085be506abfc3f92
Lucie Perruchaud <lucie.perruchaud@gmail.com>
2020-01-06 09:58:24 +0100

    commencement et correction de bug MeteoMars

--- OuterSpace/Component/MeteoMars.js
+++ OuterSpace/Component/MeteoMars.js
@@ -1,31 +1,49 @@
 // afficher la meteo sur mars
 import React from "react";
-
-import {
-  StyleSheet,
-  Text,
-  View,
-  Table,
-  Image,
-  TextInput,
-  FlatList,
-  Button,
-  Linking,
-  WebView
-} from "react-native";
+import {  StyleSheet,  Text,  View,  Table,  Image,  TextInput,  FlatList,  Button,  Linking,  WebView } from "react-native";
 
 export default class Search extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        apikey: "PAQslsDyD4gonPcnIngcaOv2w9wfJXYAysOEi5OY",
+        infos: [],
+        isLoading: true,
+        solKeys: []
+      };
+    }
+    
     render() {
-        return (
-            <View>
-                <Text>Météo de Mars</Text>
-                <Text>Dernier Jour validé</Text>
-                
-                <Text>Météo de la semaine passée</Text>
+      this.getInfos()
+      return <View>
+        <Text>Météo de Mars</Text>
+          <View>
+            <Text>Dernier Jour validé : sol n°{this.getSolKey()}</Text>
+            <Text>{this.getTemperature()}</Text>
+            <Text>Météo de la semaine passée</Text>
+            <Text>Bilan des données météorologique de la semaine</Text>
+          </View>
+        </View>
+  }
 
-                <Text>Bilan des données météorologique de la semaine</Text>
-                <Table></Table>
-            </View>
-    );
+  getInfos() {
+    return fetch("https://api.nasa.gov/insight_weather/?api_key=PAQslsDyD4gonPcnIngcaOv2w9wfJXYAysOEi5OY&feedtype=json" )
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({ infos : responseJson,
+      solKeys: responseJson.sol_keys});
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
+  getTemperature(){
+    console.log(this.infos[6]);
+    return "coucou"
+  }
+
+  getSolKey(){
+    return this.state.solKeys[6];
   }
 }
