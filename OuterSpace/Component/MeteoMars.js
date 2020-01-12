13b47da50287d94b7ae9b42b624dcbf3bc9c36d1
Lucie Perruchaud <lucie.perruchaud@gmail.com>
2020-01-12 19:16:56 +0100

    changment d'api et meteo qui marche (pas en entier, manque la semaine)

--- OuterSpace/Component/MeteoMars.js
+++ OuterSpace/Component/MeteoMars.js
@@ -6,44 +6,77 @@ export default class Search extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        apikey: "PAQslsDyD4gonPcnIngcaOv2w9wfJXYAysOEi5OY",
         infos: [],
         isLoading: true,
-        solKeys: []
+        solJour: '',
+        infosWeek: []
       };
     }
     
     render() {
-      this.getInfos()
       return <View>
         <Text>Météo de Mars</Text>
           <View>
-            <Text>Dernier Jour validé : sol n°{this.getSolKey()}</Text>
-            <Text>{this.getTemperature()}</Text>
+            <Text>Latest Weather at Elysium Planitia</Text>
+            <Text>InSight is taking daily weather measurements (temperature, wind, pressure) on the surface of Mars at Elysium Planitia, a flat, smooth plain near Mars’ equator. </Text>
+            <Text>sol : {this.state.solJour}, saison : {this.state.infos.season}</Text>
+            <Text>Temperature</Text>
+            {this.getTemperature()}
             <Text>Météo de la semaine passée</Text>
+
+            <FlatList
+              data= {this.state.infosWeek}
+              keyExtractor = {item => item.id}
+              renderItem={({item}) =>
+                <View>
+                  <Text>sol : {this.getDate(item.volumeInfo.publishedDate)}</Text>
+                  <Text>average temperature : {item.air.temperature.average }...</Text>
+                </View>
+              }
+            />
             <Text>Bilan des données météorologique de la semaine</Text>
+
           </View>
         </View>
   }
 
-  getInfos() {
-    return fetch("https://api.nasa.gov/insight_weather/?api_key=PAQslsDyD4gonPcnIngcaOv2w9wfJXYAysOEi5OY&feedtype=json" )
+  componentWillMount() {
+    return fetch("https://api.mars.spacexcompanion.app/v1/weather/latest" )
     .then((response) => response.json())
     .then((responseJson) => {
-      this.setState({ infos : responseJson,
-      solKeys: responseJson.sol_keys});
+      this.setState({
+        solJour: responseJson.sol,
+        infos: responseJson,
+        infosWeek: [],
+      });
+      for(let i = 1; i<8; i++){
+        this.getInfosWeek(i);
+      }
     })
     .catch((error) => {
       console.error(error);
     });
   }
 
-  getTemperature(){
-    console.log(this.infos[6]);
-    return "coucou"
+  getInfosWeek(i) {
+    return fetch("https://api.mars.spacexcompanion.app/v1/sols/" + (this.state.solJour-i).toString())
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.state.infosWeek.push(responseJson);
+      console.log(this.state.infosWeek)
+    })
+    .catch((error) => {
+      console.error(error);
+    });
   }
 
-  getSolKey(){
-    return this.state.solKeys[6];
+ getTemperature(){
+   if(this.state.infos.air !== undefined){
+    return <View>
+      <Text>min : {this.state.infos.air.temperature.minimum}</Text>
+      <Text>max : {this.state.infos.air.temperature.maximum}</Text>
+      <Text>moyenne : {this.state.infos.air.temperature.average}</Text>
+    </View>;
+  }
   }
 }
